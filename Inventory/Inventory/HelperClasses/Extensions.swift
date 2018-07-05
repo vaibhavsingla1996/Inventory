dcab6d98d360e298a4f85da5fb4b652de9fbf585
Vaibhav Singla <vaibhav.singla@finoit.co.in>
2018-07-05 13:00:39 +0530

    Added ProductDetailViewController

---
+++ Inventory/Inventory/HelperClasses/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Inventory
+//
+//  Created by Vaibhav Singla on 7/5/18.
+//  Copyright © 2018 Vaibhav Singla. All rights reserved.
+//
+
+import Foundation
---
+++ Inventory/Inventory/Views/ShowProductDetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShowProductDetailsTableViewCell.swift
+//  Inventory
+//
+//  Created by Vaibhav Singla on 7/5/18.
+//  Copyright © 2018 Vaibhav Singla. All rights reserved.
+//
+
+import UIKit
+
+class ShowProductDetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
